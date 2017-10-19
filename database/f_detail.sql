a962fe6b0a13d9657d7136760f2e40177222ee98
Tran Phan <tran.phan@saver.jp>
2017-10-19 15:40:01 +0700

    DB

--- database/f_detail.sql
+++ database/f_detail.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2017 at 09:10 AM
+-- Generation Time: Oct 19, 2017 at 08:38 AM
 -- Server version: 5.7.11
 -- PHP Version: 5.6.19
 
